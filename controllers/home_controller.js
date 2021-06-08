9ab8423d447ce13ff458511eeb75dc4aaf360d8f
altamas ahmed <ahmedaltamas2@gmail.com>
2021-06-08 15:58:48 +0530

    add comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,26 @@
+const { populate } = require('../models/post');
+const Post = require('../models/post');
+
+
+
 module.exports.home = function(req,res){
-    console.log(req.cookies);
-    return res.render('home',{
-        title: 'Home'
+    //   console.log(req.cookies);
+
+
+    // Post.find({}, function(err,posts){
+
+        
+    // });
+
+    Post.find({}).populate('user').exec(function(err,posts){
+        return res.render('home',{
+            title: 'Social | Home',
+            posts: posts
+        });
+
     });
+
+
+
+    
 };
