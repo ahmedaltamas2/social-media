05ff65532e8a070aed048e14a1e19b6ed9cbfbbe
altamas ahmed <ahmedaltamas2@gmail.com>
2021-06-08 12:56:19 +0530

    create post schema

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -8,12 +8,26 @@ module.exports.profile = function(req,res){
 
 
 module.exports.signUp = function(req,res){
+
+    if(req.isAuthenticated()){
+      return   res.redirect('/users/profile');
+    }
+
+
     return res.render('user_sign_up',{
         title: 'social | Sign Up'
     });
 };
 
 module.exports.signIn = function(req,res){
+
+    if(req.isAuthenticated()){
+     return   res.redirect('/users/profile');
+    }
+
+
+
+
     return res.render('user_sign_in',{
         title: 'social | Sign In'
     });
@@ -49,8 +63,17 @@ module.exports.create = function(req,res){
 }
 
 module.exports.createSession = function(req,res){
-
+    return res.redirect('/');
 };
 
 
 
+module.exports.destroySession = function(req,res){
+
+    req.logout();
+
+    return res.redirect('/');
+
+
+
+}
--- index.js
+++ index.js
@@ -6,7 +6,20 @@ const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
 
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'extended',
+    prefix: '/css'
+}));
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -24,11 +37,42 @@ app.set('layout extractScripts',true);
 
 
 
-//use express router
-app.use('/',require('./routes'));
+
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(session({
+    name:'social',
+    secret: 'something',
+    saveUninitialized: false,
+    resave:false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    },
+     store: new MongoStore(
+         {
+            mongooseConnection: db, 
+            autoRemove: 'disabled'
+         },
+    function(err){
+        console.log(err || 'connect-mongodb setup ok');
+    }
+
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(passport.setAuthenticatedUser);
+
+//use express router
+app.use('/',require('./routes'));
+
+
+
 
 
 
