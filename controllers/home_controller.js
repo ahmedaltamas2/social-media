f9ca7feba27cb8e92894dc16821d0223439fbf14
altamas ahmed <ahmedaltamas2@gmail.com>
2021-06-21 13:57:51 +0530

    add async and await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,10 @@
 const { populate } = require('../models/post');
 const Post = require('../models/post');
+const User = require('../models/user');
 
 
 
-module.exports.home = function(req,res){
+module.exports.home = async function(req,res){
     //   console.log(req.cookies);
 
 
@@ -12,21 +13,29 @@ module.exports.home = function(req,res){
         
     // });
 
-    Post.find({})
+    try{
+
+        let posts = await Post.find({})
     .populate('user')
     .populate({
         path: 'comments',
          populate: { path: 'user'}
-    })
-    .exec(function(err,posts){
-        return res.render('home',{
-            title: 'Social | Home',
-            posts: posts
-        });
+    });
+    
+    let users = await User.find({});
 
+
+    return res.render('home',{
+        title: 'Social | Home',
+        posts: posts,
+        all_users: users
     });
 
 
 
-    
+    } catch(err){
+        console.log('Error',err);
+    }
+
+   
 };
--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,13 @@ const passport = require('passport');
 
 
 const usersController = require('../controllers/users_controllers');
+router.get('/profile/:id',passport.checkAuthentication,usersController.profile);
+router.post('/update/:id',passport.checkAuthentication,usersController.update);
 
 
-router.get('/profile',passport.checkAuthentication,usersController.profile);
+
+
+
 
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
