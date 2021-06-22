acfa3deec25e7d9ab705393b4b602cf5f523450d
altamas ahmed <ahmedaltamas2@gmail.com>
2021-06-22 12:40:22 +0530

    add ajax in posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -4,10 +4,20 @@ const Comment = require('../models/comment');
 module.exports.create = async function(req,res){
 
     try{
-        await Post.create({
+       let post = await Post.create({
             content : req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post: post
+                },
+                message: "Post created!"
+              });
+        }
+
         req.flash('success','Post published')
         return res.redirect('back');
     }catch(err){
@@ -27,6 +37,18 @@ module.exports.destroy = async function(req,res){
             post.remove();
 
           await  Comment.deleteMany({post: req.params.id});
+
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id : req.params.id
+                    },
+                    message: "Post deleted successfully"
+                });
+            }
+
+
 
           req.flash('success','Post and associated comments deleted');
                 return res.redirect('back');
